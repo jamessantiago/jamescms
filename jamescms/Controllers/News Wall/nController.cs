36cff4568df38f0d1e137dbec66df3876f07cb58
jamessantiago <james.r.santiago@gmail.com>
2013-01-25 23:27:16 +0900

    fix untracked files

---
+++ jamescms/Controllers/News Wall/nController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jamescms.Controllers.News_Wall
+{
+    public class nController : Controller
+    {
+        //
+        // GET: /n/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ jamescms/Controllers/Portal/aController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jamescms.Controllers.Portal
+{
+    public class aController : Controller
+    {
+        //
+        // GET: /a/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
