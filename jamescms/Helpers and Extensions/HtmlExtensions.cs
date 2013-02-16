f2b9332a6a1714c7383aa274de8def7bba2fd2d4
jamessantiago <james.r.santiago@gmail.com>
2013-02-16 20:15:58 +0900

    html extensions

--- jamescms/Helpers and Extensions/HtmlExtensions.cs
+++ jamescms/Helpers and Extensions/HtmlExtensions.cs
@@ -5,30 +5,114 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 using System.Web.Razor;
+using System.Web.Routing;
+using System.Text;
+using System.Collections;
 using System.Web.WebPages;
+using System.ComponentModel;
 
 namespace System.Web.Mvc
 {
     public static class HtmlExtensions
     {
 
-        public static MvcHtmlString AjaxButton(this HtmlHelper helper, string linkText, string target, string action)
+        public static MvcHtmlString AjaxButton(this HtmlHelper helper, string buttonText, string target, string action)
         {
             string controller = helper.ViewContext.RouteData.GetRequiredString("controller");
-            return AjaxButton(helper, linkText, target, action, controller);
+            return AjaxButton(helper, buttonText, target, action, controller, null);
         }
 
-        public static MvcHtmlString AjaxButton(this HtmlHelper helper, string linkText, string target, string action, string controller)
+        public static MvcHtmlString AjaxButton(this HtmlHelper helper, string buttonText, string target, string action, object routeValues)
+        {
+            string controller = helper.ViewContext.RouteData.GetRequiredString("controller");
+            return AjaxButton(helper, buttonText, target, action, controller, routeValues);
+        }
+
+        public static MvcHtmlString AjaxButton(this HtmlHelper helper, string buttonText, string target, string action, string controller, object routeValues)
         {            
             var anchor = new TagBuilder("input");
             anchor.Attributes["type"] = "button";
-            string url = UrlHelper.GenerateUrl("Default", action, controller, null, null, null, false);
-            anchor.Attributes["href-to-load"] = url;
+            anchor.Attributes["href-to-load"] = UrlHelper.GenerateUrl("Default", action, controller, new RouteValueDictionary(routeValues), RouteTable.Routes, helper.ViewContext.RequestContext, false);
             anchor.Attributes["target"] = "#" + target;
+            anchor.Attributes["href-loading"] = UrlHelper.GenerateContentUrl("~/images/loading.gif", helper.ViewContext.HttpContext);
+            anchor.Attributes["value"] = buttonText;
             foreach (string css in new string[] {"jLoad", "form-button"})
-                anchor.AddCssClass(css);
-            anchor.SetInnerText(linkText);
+                anchor.AddCssClass(css);            
             return MvcHtmlString.Create(anchor.ToString());
+        }
+
+        public static MvcHtmlString AjaxReadyLoadDiv(this HtmlHelper helper, string id, string action)
+        {
+            string controller = helper.ViewContext.RouteData.GetRequiredString("controller");
+            return AjaxReadyLoadDiv(helper, id, action, controller, null);
+        }
+
+        public static MvcHtmlString AjaxReadyLoadDiv(this HtmlHelper helper, string id, string action, object routeValues)
+        {
+            string controller = helper.ViewContext.RouteData.GetRequiredString("controller");
+            return AjaxReadyLoadDiv(helper, id, action, controller, routeValues);
+        }
+
+        public static MvcHtmlString AjaxReadyLoadDiv(this HtmlHelper helper, string id, string action, string controller, object routeValues)
+        {
+            var anchor = new TagBuilder("div");
+            anchor.Attributes["id"] = id;
+            anchor.Attributes["href-to-load"] = UrlHelper.GenerateUrl("Default", action, controller, new RouteValueDictionary(routeValues), RouteTable.Routes, helper.ViewContext.RequestContext, false);
+            anchor.Attributes["href-loading"] = UrlHelper.GenerateContentUrl("~/images/loading.gif", helper.ViewContext.HttpContext);
+            anchor.AddCssClass("readyLoad");
+            return MvcHtmlString.Create(anchor.ToString());
+        }
+
+        public static string ToAttributeList(this object list)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (list != null)
+            {
+                Hashtable attributeHash = GetPropertyHash(list);
+                string resultFormat = "{0}=\"{1}\" ";
+                foreach (string attribute in attributeHash.Keys)
+                {
+                    sb.AppendFormat(resultFormat, attribute.Replace("_", ""),
+                        attributeHash[attribute]);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static string ToAttributeList(this object list,
+                                             params object[] ignoreList)
+        {
+            Hashtable attributeHash = GetPropertyHash(list);
+
+            string resultFormat = "{0}=\"{1}\" ";
+            StringBuilder sb = new StringBuilder();
+            foreach (string attribute in attributeHash.Keys)
+            {
+                if (!ignoreList.Contains(attribute))
+                {
+                    sb.AppendFormat(resultFormat, attribute,
+                        attributeHash[attribute]);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static Hashtable GetPropertyHash(object properties)
+        {
+            Hashtable values = null;
+
+            if (properties != null)
+            {
+                values = new Hashtable();
+                PropertyDescriptorCollection props =
+                    TypeDescriptor.GetProperties(properties);
+
+                foreach (PropertyDescriptor prop in props)
+                {
+                    values.Add(prop.Name, prop.GetValue(properties));
+                }
+            }
+            return values;
         }
     }
 }
--- jamescms/Scripts/master.js
+++ jamescms/Scripts/master.js
@@ -1,4 +1,29 @@
 ﻿$(document).ready(function () {
     prettyPrint();
+    jLoad();
+    readyLoad();
 });
 
+function jLoad() {
+    $(".jLoad").click(function () {
+        $($(this).attr("target")).html("<img src='" + $(this).attr("href-loading") + "' alt='loading' /> Loading");
+        $($(this).attr("target")).load($(this).attr("href-to-load"))
+    });
+}
+
+function readyLoad()
+{
+    $(".readyLoad").each(function () {
+        var currHtml = $(this).html()
+        if (currHtml.length == 0 || currHtml == "")
+        {
+            $(this).html("<img src='" + $(this).attr("href-loading") + "' alt='loading' /> Loading");
+            $(this).load($(this).attr("href-to-load"), function ()
+            {
+                prettyPrint();
+                jLoad();
+                readyLoad();
+            });
+        }        
+    });
+}
