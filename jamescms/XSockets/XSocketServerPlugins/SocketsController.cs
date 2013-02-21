fd3a84e3f1967f24a3e69347c8ee4f432fac74a5
jamessantiago <james.r.santiago@gmail.com>
2013-02-21 22:13:25 +0900

    broken again

---
+++ jamescms/XSockets/XSocketServerPlugins/SocketsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XSockets;
+using XSockets.Core.XSocket;
+using System.Diagnostics;
+
+namespace XSocketServerPlugins
+{
+    public class SocketsController : XSocketController
+    {
+
+        public SocketsController()
+        {
+            Debug.AutoFlush = true;
+        }
+
+        public void noBinding()
+        {
+            Debug.WriteLine("no binding called");
+        }
+    }
+}
