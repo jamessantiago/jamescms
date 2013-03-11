0b989eb2fb5c8f96d696fd10866b1a9ef3b528ef
jamessantiago <james.r.santiago@gmail.com>
2013-03-12 00:22:20 +0900

    quiz work

---
+++ jamescms/Models/QuizGameModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace jamescms.Models
+{
+    public class QuizGameContext : DbContext
+    {
+        public QuizGameContext()
+            : base("ApplicationConnection")
+        {
+        }
+
+
+    }
+
+    public class QuizState : Entity
+    {
+        public int LastQuestion { get; set; }
+        public DateTime LastTimeStarted { get; set; }        
+    }
+
+    public class TriviaQuestion : Entity
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+
+    public class UserGameProfile : Entity
+    {
+        public int AccountModelUserId { get; set; }
+        public DateTime GameAccountCreated { get; set; }
+        public DateTime LastTimeSeen { get; set; }
+        public string Nickname { get; set; }
+        public long Points { get; set; }
+    }
+}
---
+++ jamescms/Services/WebSocketControllers/WebSocketQuizGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using jamescms.Helpers;
+using jamescms.Models;
+using System.IO;
+using NLog;
+using Fleck;
+using System.Net;
+
+namespace jamescms.Services.WebSocketControllers
+{
+    public class WebSocketQuizGame
+    {
+        private Logger logger = LogManager.GetLogger("WebSocketQuizGame");
+        private WebSocketServer server;
+
+        public void Start()
+        {
+            server = new WebSocketServer("ws://santiagodevelopment.com:8990/" + serverName);
+            try
+            {
+                server.Start(socket =>
+                {
+                    socket.OnOpen = () => 
+                    socket.OnError = error => logger.DebugException("Error occurred establishing new websocket", error);
+                    socket.OnMessage = 
+                });
+            }
+            catch (Exception ex)
+            {
+                logger.DebugException("Failed to establish file tail web socket", ex);
+            }
+        }
+
+        public 
+    }
+}
